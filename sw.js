c70758e348c3731586bc84913a93e92b2d5c6d56
vistaprods-projects <vistaprod.io@gmail.com>
2026-02-17 16:15:20 +0100

    feat: Add calculator tab with efficiency calculation

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'pyl-calc-v2';
+const ASSETS = [
+    './',
+    './index.html',
+    './manifest.json',
+    './icon.svg'
+];
+
+self.addEventListener('install', event => {
+    self.skipWaiting();
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.filter(name => name !== CACHE_NAME)
+                    .map(name => caches.delete(name))
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
